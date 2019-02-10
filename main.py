f77fb5d718dc23a80398541c3f7e50ba22411b62
Ardavan Shafiee <bm.bigbang@gmail.com>
2019-02-10 20:26:26 +0000

    fixing issue with stat gathering and finding extremeties in the data. first repo commit.

--- main.py
+++ main.py
@@ -35,22 +35,43 @@ next_batch[10000:11000].plot(x='time_to_failure', y='acoustic_data', ax=axs[0, 1
 next_batch[10000:10300].plot(x='time_to_failure', y='acoustic_data', ax=axs[1, 0])
 next_batch[10000:10100].plot(x='time_to_failure', y='acoustic_data', ax=axs[1, 1])
 
-max_acoustic = next_batch.max(axis=0)[0]
-min_acoustic = next_batch.min(axis=0)[0]
-max_time = next_batch.max(axis=0)[1]
-min_time = next_batch.min(axis=0)[1]
-while not end_of_file:
+if next_batch.empty:
+    train_data_generator = DataGen(file_path='train.csv', chunk_size=10000000)
     next_batch, end_of_file = train_data_generator.next_batch()
-    temp = next_batch.max(axis=0)[0]
-    if temp > max_acoustic:
-        max_acoustic = temp
-    temp = next_batch.min(axis=0)[0]
-    if temp < min_acoustic:
-        min_acoustic = temp
-    temp = next_batch.max(axis=0)[1]
-    if temp > max_time:
-        max_time = temp
-    temp = next_batch.min(axis=0)[1]
-    if temp > min_time:
-        min_time = temp
 
+maxx = next_batch.max(axis=0)
+minn = next_batch.min(axis=0)
+summ = next_batch.sum(axis=0)
+max_acoustic = maxx[0]
+sum_acoustic = summ[0]
+min_acoustic = minn[0]
+max_time = maxx[1]
+sum_time = summ[1]
+min_time = minn[1]
+data_points = next_batch.shape[0]
+while True:
+    maxx = next_batch.max(axis=0)
+    minn = next_batch.min(axis=0)
+    if maxx[0] > 5000.0:
+        next_batch.plot(x='time_to_failure', y='acoustic_data', label='anomalous large max acoustic spike')
+    if maxx[0] > max_acoustic:
+        max_acoustic = maxx[0]
+    if minn[0] < min_acoustic:
+        min_acoustic = minn[0]
+    if maxx[1] > max_time:
+        max_time = maxx[1]
+    if minn[1] < min_time:
+        min_time = minn[1]
+
+    next_batch, end_of_file = train_data_generator.next_batch()
+    if end_of_file:
+        break
+    data_points += next_batch.shape[0]
+    summ = next_batch.sum(axis=0)
+    sum_acoustic += summ[0]
+    sum_time += summ[1]
+
+stats = {
+    'max_acoustic': max_acoustic, 'min_acoustic': min_acoustic, 'max_time': max_time, 'min_time': min_time,
+    'avg_acoustic': sum_acoustic / data_points, 'ave_time': sum_time / data_points
+}
